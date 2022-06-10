a6161d153488049fa4701da6fda808015d1162a1
Hei5enberg44 <antoineboulet44@gmail.com>
2022-06-10 14:06:54 +0200

    Mise à jour du endpoint servant à vérifier le token 2FA

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const speakeasy = require('speakeasy')
 const QRCode = require('qrcode')
 const express = require('express')
-const cookieSession = require('cookie-session')
 const app = express()
-const port = 3005
+const port = 3002
 
 app.set('view engine', 'ejs')
-app.use(cookieSession({
-    name: 'session',
-    keys: ['YbwHwELe229SKZUQ']
-}))
 app.use(express.json())
 
 function validateToken(token, secret) {
     const tokenValidates = speakeasy.totp.verify({
-        secret: secret.base32,
+        secret: secret,
         encoding: 'base32',
         token: token
     })
-
     return tokenValidates
 }
 
@@ -27,8 +21,6 @@ app.get('/', async (req, res) => {
         length: 10,
         name: 'SylverApp'
     })
-
-    req.session.secret = secret
 
     const qr = await QRCode.toDataURL(secret.otpauth_url)
 
@@ -40,14 +32,13 @@ app.get('/', async (req, res) => {
 
 app.post('/validateToken', (req, res) => {
     const token = req.body.token
+    const secret = req.body.secret
 
-    if(token) {
-        res.json({ valid: validateToken(token, req.session.secret) })
+    if(token && secret) {
+        res.json({ valid: validateToken(token, secret) })
     } else {
         res.status(404).send('Token manquant dans la requête')
     }
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(port)
